4c9eda94f6f91f08f53034ab1bfba30232be101b
Ajit Vijayakumar <ajitvijayakumar@umail.ucsb.edu>
2019-05-29 16:31:04 -0700

    client functions added

--- client.py
+++ client.py
@@ -5,42 +5,15 @@ import time
 import ast
 
 
+def get_connection(ip_address, port_server):
+	client_socket = socket(AF_INET, SOCK_STREAM)
+	client_socket.connect((ip_address,port_server))
+	return client_socket
 
-def bindServer(startingPort,procNum,configFile):
-	connect = False
-	while not connect:
-		try:
-
-		    connection = serverListener.bind(('', startingPort))
-			connect = True
-		except socket.error as err:
-			startingPort = startingPort + 1
-			pass
-	#read the dict, add new port number to it
-	configDict = {}
-	with open(configFile, "r") as config:
-        lineList = myfile.readlines()
-	    configDict = ast.literal_eval(str(lineList[-1]))
-	    configDict[str(procNum)] = startingPort
-	# add updated dict to config
-	with open(configFile,"w") as config:
-		config,write(str(configDict))
-	return connection
-
-#will be run concurrently to connectToLessThanServers. End result is two dicts that are combined.
-def waitToReceiveConnections(portNum,procNum,maxProc):
-	receivedCount = 0
-	connections = {}
-	while receivedCount < maxProc - procNum:
-	    sock,addr = serverListener.accept()
-	    procID = sock.recv(1024).decode('utf-8')
-	    connections[str(procID)] = sock
-	return connections	
-
-def connectToLessThanServers(procNum,ports):
-	connections = {}
-
-	for proc in range(procNum-1,1):
-		    connections[str(proc)] = sock.connect(('127.0.0.1', ports[str(proc)]))
-			connections[str(proc)].send(bytes(procNum))
-	return connections
+def listen(connection):
+	try:
+		while True:
+			msg = connection.recv(1024)
+			print msg
+	finally:
+		connection.close()
---
+++ networkprocess.py
@@ -0,0 +1,5 @@
+import random
+import socket
+import threading
+import time
+import ast
