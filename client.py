3c1431517441f926907f7b75080897d1d9c996a5
jonathanday930 <jday.day03@gmail.com>
2019-06-06 12:50:29 -0700

    Update client.py

--- client.py
+++ client.py
@@ -7,11 +7,43 @@ import config
 import ast
 import hashlib
 
-def get_connection(ip_address, port_server):
-	client_socket = socket(AF_INET, SOCK_STREAM)
-	#client_socket.bind((config.server_ipaddress, config.client_port))
-	client_socket.connect((ip_address,port_server))
-	return client_socket
+
+def connectToNetwork(proc_num):
+	NWSock = socket(AF_INET, SOCK_STREAM)
+
+	while True:
+		try:
+			NWSock.connect(('127.0.0.1', config.serverPortNumber))
+			break
+		except:
+			pass
+	NWSock.send(bytes(str(proc_num), encoding='utf8'))
+	return NWSock
+
+def turnLetterIntoNum(letter):
+	if letter.upper() == 'A':
+		return 0
+	else: 
+		if letter.upper() == 'B':
+			return 1
+		else:
+			if letter.upper() == 'C':
+				return 2
+			else:
+				if letter.upper() == 'D':
+					return 3
+				else:
+					if letter.upper() == 'E':
+						return 4
+
+def sendTransaction(transaction, NWSock):
+	newMessage = {}
+	newMessage['type'] = 'transaction'
+	newMessage['sender'] = -1
+	newMessage['destination'] = turnLetterIntoNum(transaction[0])
+	newMessage['transaction'] = transaction
+	NWSock.send(bytes(str(newMessage) , encoding='utf8'))
+
 
 def listen(connection):
 	try:
@@ -26,7 +58,7 @@ def listen(connection):
 # carol = get_connection(config.server_ipaddress, config.carol_port)
 # devon = get_connection(config.server_ipaddress, config.devon_port)
 # elizabeth = get_connection(config.server_ipaddress, config.elizabeth_port)
-client = get_connection(config.server_ipaddress, config.client_port)
+NWSock = connectToNetwork(-1)
 #_thread.start_new_thread(listen, (client, ))
 
 # _thread.start_new_thread(listen, (alice, ))
@@ -39,8 +71,9 @@ while(1):
 	transaction = input("Enter Transaction: ")
 	try:
 		(sender, receiver, value) = transaction.split()
+		transaction = (sender, receiver, value)
 	except ValueError:
 		print("invalid input format. must be Sender Receiver Money")
 	if sender and receiver and value:
-		client.send(transaction.encode())
+		sendTransaction(transaction,NWSock)
 		print("client sent transaction")
