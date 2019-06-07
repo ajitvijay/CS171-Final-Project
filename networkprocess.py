9925186fde1fda2591d40f45b5c2eeeb1f64914b
Ajit Vijayakumar <ajitvijayakumar@umail.ucsb.edu>
2019-06-06 20:19:29 -0700

    debugging

--- networkprocess.py
+++ networkprocess.py
@@ -9,7 +9,7 @@ import hashlib
 
 def sendMessageWithDelay(message, destination,destinationInt):
     sleepAmnt = random.uniform(0, 3)
-    print('Sending message: ' + str (message))
+    # print('Sending message: ' + str (message))
     if int(destinationInt) >0:
         time.sleep(sleepAmnt)
     destination.send(bytes(message + '%',encoding='utf-8'))
@@ -61,7 +61,7 @@ def startNetwork():
         try:
 
             sock, addr1 = serverListener.accept()
-            procID = sock.recv(1024).decode('utf-8')
+            procID = sock.recv(4096).decode('utf-8')
 
             print('Started connection with server: ' + procID)
 
@@ -73,7 +73,7 @@ def startNetwork():
 # Send messages with delay
         for socketVar in serverSockets.values():
             try:
-                messageString = socketVar.recv(1024).decode('utf-8')
+                messageString = socketVar.recv(4096).decode('utf-8')
 
                 if messageString != '':
                     messages = separateMessages(messageString)
@@ -81,9 +81,9 @@ def startNetwork():
                         print('Received message: ' + message)
 
                         messageDict = ast.literal_eval(message)
-
-                        sendThread = threading.Thread(target=sendMessageWithDelay, args=(message, serverSockets[str(messageDict['destination'])],messageDict['destination']) )
-                        sendThread.start()
+                        if str(messageDict['destination']) in serverSockets:
+                            sendThread = threading.Thread(target=sendMessageWithDelay, args=(message, serverSockets[str(messageDict['destination'])],messageDict['destination']) )
+                            sendThread.start()
 
             except socket.error as err:
                 pass
